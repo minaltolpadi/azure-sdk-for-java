fb40b5e80733d0fb06a8512ceaf2f46f1e1108c9
Connie <conniey@microsoft.com>
2019-09-10 16:32:52 -0700

    Adding ClientConstants.NOT_APPLICABLE for SessionHandler.

---
+++ sdk/core/azure-core-amqp/src/main/java/com/azure/core/amqp/implementation/ClientConstants.java
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.core.amqp.implementation;
+
+public class ClientConstants {
+    public static final String NOT_APPLICABLE = "n/a";
+}
